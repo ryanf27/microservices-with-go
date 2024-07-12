27728aa092f61e1a8ca3fb26f818a45c34b76ea7
ryanf27 <ryanfebry0227@gmail.com>
2024-07-12 23:16:25 +0700

    create user model

---
+++ userservice/pkg/models/user.go
@@ -0,0 +1,44 @@
+package models
+
+import (
+	"userservice/pkg/config"
+
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB
+
+type User struct {
+	gorm.Model
+	Username  string `json:"username"`
+	Email string `json:"email"`
+}
+
+func init() {
+	config.Connect()
+	db = config.GetDB()
+	db.AutoMigrate(&User{})
+}
+
+func (b *User) CreateUser() *User {
+	db.Create(&b)
+	return b
+}
+
+func GetAllUsers() []User {
+	var  Users []User
+	db.Find(&Users)
+	return Users
+}
+
+func GetUserById(Id int64) (*User, *gorm.DB) {
+	var getUser User
+	db := db.Where("ID=?", Id).Find(&getUser)
+	return &getUser, db
+}
+
+func DeleteUser(ID int64)User{
+	var user User
+	db.Where("ID=?", ID).Delete(user)
+	return user
+}
